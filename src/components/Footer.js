1b3d2f16b7fdcacd84b503d319fe0b7503cefd74
Anle101 <46081192+Anle101@users.noreply.github.com>
2021-05-19 19:55:21 -0400

    Added working links and a profile image

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,21 +2,21 @@ import React from 'react';
 import {Button} from './Button';
 import './Footer.css';
 import {Link} from 'react-router-dom';
+import Pdf from '../Documents/Resume2021.pdf';
+
 function Footer() {
     return (
         <div className= 'footer-container'>
-           
             <section className="social-media">
                 <div className="social-media-wrap">
                     <div className="social-icons">
-                        <Link to='/' className="social-icon-link"><i className="fas fa-file"></i></Link>
-                        <Link to='/' className="social-icon-link"><i className="fab fa-linkedin"></i></Link>
-                        <Link to='/' className="social-icon-link"><i className="fab fa-instagram"></i></Link>
-                        <Link to='/' className="social-icon-link"><i className="fab fa-facebook-f"></i></Link>
+                         <a href={Pdf} target="_blank" rel='noopener noreferrer' className="social-icon-link"><i className="fas fa-file"></i></a>
+                         <a href="https://www.linkedin.com/in/anle102/" className="social-icon-link"><i className="fab fa-linkedin"></i></a>
+                         <a href="https://www.instagram.com/an_le_132/" className="social-icon-link"><i className="fab fa-instagram"></i></a>
+                         <a href="https://www.facebook.com/profile.php?id=100006228614156" className="social-icon-link"><i className="fab fa-facebook-f"></i></a>            
                     </div>
                 </div>
             </section>
-           
         </div>
 
     );
--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -2,12 +2,14 @@ import React from 'react';
 import '../App.css';
 import { Button } from './Button';
 import './MainSection.css';
+import logo from './Profile.jpg';
 
 function MainSection() {
     return (
         <div className='main-container'>
             <h1>Website Developer</h1>
-        
+
+            <img src={logo} className="picture"/>
             <div className="main-btns">
 
                 <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
