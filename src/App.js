e067249828ff0fd2fd15e762416a4cda7293d546
Anle101 <46081192+Anle101@users.noreply.github.com>
2021-06-10 18:38:32 -0400

    Fixed homepage issue

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
-import { render } from 'react-dom';
+
 import React, {useState, useEffect} from 'react'; 
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home';
-import Skills from './components/Skills';
-import Experience from './components/Experience';
+
 import Footer from './components/Footer';
 import { ParallaxProvider } from 'react-scroll-parallax';
+import { render } from '@testing-library/react';
 
-function App() {
+class App extends React.Component {
 
-    document.title = "An Le";
-    return (
-        <>
-         <ParallaxProvider>
-            <Router>
-                <Navbar />
-                <Switch >
-                    <Route path='/' exact component = {Home} />
-                    <Route path='/skills' exact component = {Skills} />
-                    <Route path='/experience' exact component = {Experience} />
-                </Switch>
-                <Footer />
-            </Router>
-         </ParallaxProvider>
-        </>
-    );
+   
+    render() {
+        document.title = "An Le";
+        return (
+            <>
+            <ParallaxProvider>
+                <Router>
+                    <Navbar />
+                    <Switch >
+                        <Route path='/' exact ={true} component = {Home} />
+                    
+                    </Switch>
+                    <Footer />
+                </Router>
+            </ParallaxProvider>
+            </>
+        );
+    }
+    
 }
 
 export default App;
